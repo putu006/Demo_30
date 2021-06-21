e36fc2b504b42fd9e90ee1551ee8db81d68e52d3
abhishek <abhi3rajput@gmail.com>
2021-06-21 21:20:12 +0530

    Assignment

---
+++ assign5.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/info', methods=['GET'])
+def testget():
+    return jsonify({'Receiver':'Cisco is the best!'})
+
+
+@app.route('/ping', methods=['POST'])
+def testpost():
+     input_json = request.get_json()                        # {"url": "http://api.plos.org/search?q=title:DNA"}
+     print(input_json['url'])                               # {"url": "http://ergast.com/api/f1/2004/1/results.json"}
+     res = requests.get(input_json['url']).json()
+     return jsonify(res)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
